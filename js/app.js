0a65b38f9cdaac90a8ce41c1218fb95f3c6aee1e
Aathif Hidayathulla <crazybiker696@gmail.com>
2024-09-03 11:41:22 +0530

    button add

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ const listBookButton = document.querySelector('.fetch-books-btn');
 listBookButton.addEventListener('click', () => {
 	console.log('Button clicked');
 })
+
+const listAuthorButton = document.querySelector('.fetch-authors-btn');
+
+listAuthorButton.addEventListener('click', () => {
+	console.log('Button clicked');
+})
